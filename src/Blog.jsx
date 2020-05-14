6ba9e48060319ba6ae4feb1aa872572fc69e07e8
akine <weezer.and.you@gmail.com>
2020-05-14 16:32:56 +0900

    duh

---
+++ src/Blog.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Article from './Article';
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPublished: false,
+      order: 1
+    }
+  }
+
+  togglePublished = () => {
+    this.setState( {
+      isPublished: !this.state.isPublished
+    })
+  };
+
+
+  render() {
+    return (
+      <>
+        <Article title={"React how to"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()} />
+      </>
+    )
+  }
+}
+
+export default Blog
