e854e559327e252973cce0361c7b0c5b73435b60
akine <weezer.and.you@gmail.com>
2020-05-15 08:54:15 +0900

    import export duh

--- src/components/Pugera.jsx
+++ src/components/Pugera.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export default class Pugera extends React.Component {
-  render() {
-    return (<h1>Pugerap</h1>)
-  }
+// export default class Pugera extends React.Component {
+//   render() {
+//     return (<h1>Pugerap</h1>)
+//   }
+// }
+
+// default exportはアロー関数だと
+// export default FuncName ができないから、
+// const Pugera で先に宣言して
+// export default Pugeraする
+const Pugera = () => {
+  return (<h1>I'm real Pugera.</h1>)
 }
+export default Pugera;
--- src/components/UnkoKusai.jsx
+++ src/components/UnkoKusai.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-export function Unko() {
-  return(<h1>Unkoooooo</h1>)
-};
+// export function Unko() {
+//   return(<h1>Unkoooooo</h1>)
+// };
+
+// こっちがアロー関数使うやつ, 名前付き関数
+export const Unko = () => {
+  return (<h1>Unko desu 2</h1>)
+}
 
 export const Kusai = () => {
   return (<h1>Kusaiiiiii</h1>)
