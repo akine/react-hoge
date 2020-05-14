2f697bdb25969a038b0d100f675fadff708fbefd
akine <weezer.and.you@gmail.com>
2020-05-14 17:02:48 +0900

    lifecycle

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -6,9 +6,23 @@ class Blog extends React.Component {
     super(props);
     this.state = {
       isPublished: false,
-      order: 1
+      count: 0
     }
   }
+
+  componentDidMount() {
+    document.getElementById('counter').addEventListener('click', this.countUp)
+  }
+
+  componentDidUpdate() {
+    if (this.state.count >= 10) {
+      this.setState( { count: 0 })
+    }
+  }
+
+componentWillUnmount() {
+  document.getElementById('counter').removeEventListener('click', this.countUp)
+}
 
   togglePublished = () => {
     this.setState( {
@@ -16,11 +30,20 @@ class Blog extends React.Component {
     })
   };
 
+  countUp = () => {
+    this.setState( { count: this.state.count + 1 })
+  }
+
 
   render() {
     return (
       <>
-        <Article title={"React how to"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()} />
+        <Article 
+          title={"React how to"} 
+          isPublished={this.state.isPublished} 
+          toggle={() => this.togglePublished()}
+          count={this.state.count}
+        />
       </>
     )
   }
