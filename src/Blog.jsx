c837d3e9f8a0eae485d62f7d820ea15ba04744ec
akine <weezer.and.you@gmail.com>
2020-05-15 15:30:26 +0900

    hoooooooooook :art:

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,55 +3,39 @@ import Article from './Article';
 import * as Unko from './components/UnkoKusai'
 import Pugera from './components/Pugera';
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
 
-  componentDidMount() {
-    document.getElementById('counter').addEventListener('click', this.countUp)
-  }
+  //   componentDidMount() {
+  //     document.getElementById('counter').addEventListener('click', this.countUp)
+  //   }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState( { count: 0 })
-    }
-  }
+  //   componentDidUpdate() {
+  //     if (this.state.count >= 10) {
+  //       this.setState( { count: 0 })
+  //     }
+  //   }
 
-componentWillUnmount() {
-  document.getElementById('counter').removeEventListener('click', this.countUp)
-}
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', this.countUp)
+  // }
 
-  togglePublished = () => {
-    this.setState( {
-      isPublished: !this.state.isPublished
-    })
-  };
-
-  countUp = () => {
-    this.setState( { count: this.state.count + 1 })
-  }
+  // countUp = () => {
+  //   this.setState( { count: this.state.count + 1 })
+  // }
 
 
-  render() {
-    return (
-      <>
-        <Article 
-          title={"React how to"} 
-          isPublished={this.state.isPublished} 
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-        <Unko.Unko />
-        <Unko.Kusai />
-        <Pugera />
-      </>
-    )
-  }
+
+  return (
+    <>
+      <Article
+        title={"React how to"}
+        // count={this.state.count}
+      />
+      <Unko.Unko />
+      <Unko.Kusai />
+      <Pugera />
+    </>
+  )
 }
 
 export default Blog
